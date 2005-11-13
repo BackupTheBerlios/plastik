ba17b952a38b02cdae51d1ef42613c187ebce32f
tobain <tobain>
2005-11-13 14:09:56 +0000

    First design of this interface, but it needs discussion...

--- plastik/src/de/hampelratte/plastik/theme/PlastikColorTheme.java
+++ plastik/src/de/hampelratte/plastik/theme/PlastikColorTheme.java
@@ -1,5 +1,27 @@
 package de.hampelratte.plastik.theme;
 
-public interface PlastikColorTheme {
+import java.awt.Color;
 
+public interface PlastikColorTheme {
+	
+	// Types
+	public static final int TYPE_BACKGROUND = 0;
+	public static final int TYPE_FOREGROUND = 1;
+	public static final int TYPE_ROLLOVER   = 2;
+	public static final int TYPE_FOCUS      = 3;
+	
+	// Options
+	// (günstig für Arrayimplementierung)
+	public static final int NORMAL        = 0;  // ... 0000 0000  0000 0000
+	public static final int BRIGHTER      = 1;  // ... 0000 0000  0000 0001
+	public static final int BRIGHTER_MORE = 2;  // ... 0000 0000  0000 0010
+	public static final int DARKER        = 3;  // ... 0000 0000  0000 0011
+	public static final int DARKER_MORE   = 4;  // ... 0000 0000  0000 0100
+	
+	public static final int LIGHT_MASK    = 7;  // ... 0000 0000  0000 0111
+	
+	public static final int INACTIVE_MASK = 16; // ... 0000 0000  0001 0000
+	public static final int ROLLOVER_MASK = 32; // ... 0000 0000  0010 0000
+	
+	public Color getButtonColor(int type, int options);
 }
