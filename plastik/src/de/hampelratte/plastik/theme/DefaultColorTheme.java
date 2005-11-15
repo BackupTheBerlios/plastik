5b72176eed4f8e8dfe08db8d0663babb7f1ffb35
tobain <tobain>
2005-11-15 23:11:32 +0000

    color changes

--- plastik/src/de/hampelratte/plastik/theme/DefaultColorTheme.java
+++ plastik/src/de/hampelratte/plastik/theme/DefaultColorTheme.java
@@ -7,14 +7,14 @@ public class DefaultColorTheme extends AbstractColorTheme {
 	public DefaultColorTheme() {
 		super();
 		
-		setColor(new Color(200, 200, 200), COMMON | BACKGROUND);
-		setColor(new Color(200, 200, 200), COMMON | BACKGROUND | INACTIVE);
+		setColor(new Color(239, 239, 239), COMMON | BACKGROUND);
+		setColor(new Color(239, 239, 239), COMMON | BACKGROUND | INACTIVE);
 		
-		setColor(new Color(50,50,50), COMMON | FOREGROUND);
-		setColor(new Color(50,50,50), COMMON | FOREGROUND | INACTIVE);
+		setColor(new Color(  0,   0,   0), COMMON | FOREGROUND);
+		setColor(new Color(  0,   0,   0), COMMON | FOREGROUND | INACTIVE);
 		
-		setColor(new Color(100,100,100), COMMON | BORDER);
-		setColor(new Color(100,100,100), COMMON | BORDER | INACTIVE);
+		setColor(new Color(134,134,134), COMMON | BORDER);
+		setColor(new Color(134,134,134), COMMON | BORDER | INACTIVE);
 		
 		setColor(new Color(100,200,100), COMMON | FOCUS);
 		setColor(new Color(100,200,100), COMMON | FOCUS | INACTIVE);
