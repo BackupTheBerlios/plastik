2eda170ed408b2e65cf7e48b3ab6541540599d9e
tobain <tobain>
2005-11-17 13:09:57 +0000

    changed initialisation of color, because constants have changed

--- plastik/src/de/hampelratte/plastik/theme/DefaultColorTheme.java
+++ plastik/src/de/hampelratte/plastik/theme/DefaultColorTheme.java
@@ -18,12 +18,16 @@ public class DefaultColorTheme extends AbstractColorTheme {
 		setColor( 16,  16,  16, COMMON | FOCUS                    | INACTIVE );
 		setColor(103, 141, 178, COMMON | ROLLOVER                            );
 		setColor(  0,   0,   0, COMMON | ROLLOVER                 | INACTIVE );
-		setColor(239, 239, 239, COMMON | BACKGROUND_PRESSED                  );
+		setColor(221, 223, 228, COMMON | BACKGROUND_COMPONENT                );
+		setColor(239, 239, 239, COMMON | BACKGROUND_COMPONENT     | INACTIVE );
+		setColor(  0,   0,   0, COMMON | FOREGROUND_COMPONENT                );
+		setColor(  0,   0,   0, COMMON | FOREGROUND_COMPONENT     | INACTIVE );
+		setColor(207, 209, 213, COMMON | BACKGROUND_PRESSED                  );
 		setColor(239, 239, 239, COMMON | BACKGROUND_PRESSED       | INACTIVE );
 		setColor(  0,   0,   0, COMMON | FOREGROUND_PRESSED                  );
 		setColor(  0,   0,   0, COMMON | FOREGROUND_PRESSED       | INACTIVE );
-		setColor(239, 239, 239, COMMON | BACKGROUND_TEXT                     );
-		setColor(239, 239, 239, COMMON | BACKGROUND_TEXT          | INACTIVE );
+		setColor(255, 255, 255, COMMON | BACKGROUND_TEXT                     );
+		setColor(255, 255, 255, COMMON | BACKGROUND_TEXT          | INACTIVE );
 		setColor(  0,   0,   0, COMMON | FOREGROUND_TEXT                     );
 		setColor(  0,   0,   0, COMMON | FOREGROUND_TEXT          | INACTIVE );
 		setColor(239, 239, 239, COMMON | BACKGROUND_TEXT_SELECTED            );
@@ -32,13 +36,13 @@ public class DefaultColorTheme extends AbstractColorTheme {
 		setColor(  0,   0,   0, COMMON | FOREGROUND_TEXT_SELECTED | INACTIVE );
 		
 		// BUTTON (optional)
-		setColor(221, 223, 228, BUTTON | BACKGROUND);
-		setAdjustmentValue( 10, BUTTON | BACKGROUND | BRIGHTER_GRADIENT);
-		setAdjustmentValue(-10, BUTTON | BACKGROUND | DARKER_GRADIENT);
-		
-		setColor(207, 209, 213, BUTTON | BACKGROUND_PRESSED);
-		setAdjustmentValue( -4, BUTTON | BACKGROUND_PRESSED | BRIGHTER_GRADIENT);
-		setAdjustmentValue( +4, BUTTON | BACKGROUND_PRESSED | DARKER_GRADIENT);
+//		setColor(221, 223, 228, BUTTON | BACKGROUND);
+//		setAdjustmentValue( 10, BUTTON | BACKGROUND | BRIGHTER_GRADIENT);
+//		setAdjustmentValue(-10, BUTTON | BACKGROUND | DARKER_GRADIENT);
+//		
+//		setColor(207, 209, 213, BUTTON | BACKGROUND_PRESSED);
+//		setAdjustmentValue( -4, BUTTON | BACKGROUND_PRESSED | BRIGHTER_GRADIENT);
+//		setAdjustmentValue( +4, BUTTON | BACKGROUND_PRESSED | DARKER_GRADIENT);
 		
 		
 	}
