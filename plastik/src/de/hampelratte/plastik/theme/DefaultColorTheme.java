92008ecf7ca378155b1790a18e31d34a7f8537be
tobain <tobain>
2005-11-17 20:35:24 +0000

    new colors added

--- plastik/src/de/hampelratte/plastik/theme/DefaultColorTheme.java
+++ plastik/src/de/hampelratte/plastik/theme/DefaultColorTheme.java
@@ -7,17 +7,15 @@ public class DefaultColorTheme extends AbstractColorTheme {
 	public DefaultColorTheme() {
 		super();
 		
-		// COMMON (must be set)
+	//-COMMON (must be set)-----------------------------------------------------
+		// windows
 		setColor(239, 239, 239, COMMON | BACKGROUND                          );
 		setColor(239, 239, 239, COMMON | BACKGROUND               | INACTIVE );
 		setColor(  0,   0,   0, COMMON | FOREGROUND                          );
 		setColor(  0,   0,   0, COMMON | FOREGROUND               | INACTIVE );
 		setColor(134, 134, 134, COMMON | BORDER                              );
 		setColor(134, 134, 134, COMMON | BORDER                   | INACTIVE );
-		setColor( 16,  16,  16, COMMON | FOCUS                               );
-		setColor( 16,  16,  16, COMMON | FOCUS                    | INACTIVE );
-		setColor(103, 141, 178, COMMON | ROLLOVER                            );
-		setColor(  0,   0,   0, COMMON | ROLLOVER                 | INACTIVE );
+		// components
 		setColor(221, 223, 228, COMMON | BACKGROUND_COMPONENT                );
 		setColor(239, 239, 239, COMMON | BACKGROUND_COMPONENT     | INACTIVE );
 		setColor(  0,   0,   0, COMMON | FOREGROUND_COMPONENT                );
@@ -26,6 +24,13 @@ public class DefaultColorTheme extends AbstractColorTheme {
 		setColor(239, 239, 239, COMMON | BACKGROUND_PRESSED       | INACTIVE );
 		setColor(  0,   0,   0, COMMON | FOREGROUND_PRESSED                  );
 		setColor(  0,   0,   0, COMMON | FOREGROUND_PRESSED       | INACTIVE );
+		setColor( 16,  16,  16, COMMON | FOCUS                               );
+		setColor( 16,  16,  16, COMMON | FOCUS                    | INACTIVE );
+		setColor(103, 141, 178, COMMON | ROLLOVER                            );
+		setColor(  0,   0,   0, COMMON | ROLLOVER                 | INACTIVE );
+		setColor(134, 134, 134, COMMON | BORDER_COMPONENT                    );
+		setColor(134, 134, 134, COMMON | BORDER_COMPONENT         | INACTIVE );	
+		// text
 		setColor(255, 255, 255, COMMON | BACKGROUND_TEXT                     );
 		setColor(255, 255, 255, COMMON | BACKGROUND_TEXT          | INACTIVE );
 		setColor(  0,   0,   0, COMMON | FOREGROUND_TEXT                     );
@@ -36,15 +41,10 @@ public class DefaultColorTheme extends AbstractColorTheme {
 		setColor(  0,   0,   0, COMMON | FOREGROUND_TEXT_SELECTED | INACTIVE );
 		
 		
-		// BUTTON (optional)
-//		setColor(221, 223, 228, BUTTON | BACKGROUND);
-//		setAdjustmentValue( 10, BUTTON | BACKGROUND | BRIGHTER_GRADIENT);
-//		setAdjustmentValue(-10, BUTTON | BACKGROUND | DARKER_GRADIENT);
-
-//		setColor(207, 209, 213, BUTTON | BACKGROUND_PRESSED);
+	// BUTTON (optional)--------------------------------------------------------
 		setAdjustmentValue( -4, BUTTON | BACKGROUND_PRESSED | BRIGHTER_GRADIENT);
-		setAdjustmentValue( +4, BUTTON | BACKGROUND_PRESSED | DARKER_GRADIENT);
-		setAdjustmentValue(+40, BUTTON | FOREGROUND_COMPONENT | INACTIVE | BRIGHTER);
+		setAdjustmentValue(  4, BUTTON | BACKGROUND_PRESSED | DARKER_GRADIENT);
+		setAdjustmentValue( 40, BUTTON | FOREGROUND_COMPONENT | INACTIVE | BRIGHTER);
 		setAdjustmentValue(-40, BUTTON | FOREGROUND_COMPONENT | INACTIVE | DARKER);
 		
 		
