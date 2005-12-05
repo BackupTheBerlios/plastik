8993318ffb83c3dcb417c914f56912968713bf7d
tobain <tobain>
2005-12-05 10:32:53 +0000

    many fixes (compare the versions, its more easy than to describe it :-)

--- plastik/src/de/hampelratte/plastik/Gradients.java
+++ plastik/src/de/hampelratte/plastik/Gradients.java
@@ -4,33 +4,58 @@ import java.awt.Color;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Paint;
 import java.awt.Rectangle;
 
 public class Gradients {
 	
-	public static void drawRoundBoxGradient(Graphics g, Rectangle rect, Color c1, Color c2) {
-		Graphics2D g2 = (Graphics2D)g;
-		GradientPaint gp = new GradientPaint(0,0,c1,0,rect.height,c2);
-		g2.setPaint(gp);
-		g2.fillRoundRect((int)rect.getX(), (int)rect.getY(), 
-				(int)rect.getWidth(), (int)rect.getHeight(), 4, 4);
+	public static void drawRoundBoxGradient(Graphics g, Rectangle rect, int arcWidth, int arcHeight, Color top, Color bottom) {
+		drawRoundBoxGradient(g, rect.x, rect.y, rect.width, rect.height, arcWidth, arcHeight, top, bottom);
 	}
 	
 	
-	public static void drawBoxGradient(Graphics g, Rectangle rect, Color c1, Color c2) {
+	public static void drawRoundBoxGradient(Graphics g, int x, int y, int w, int h, int arcWidth, int arcHeight, Color top, Color bottom) {
 		Graphics2D g2 = (Graphics2D)g;
-		GradientPaint gp = new GradientPaint(0,0,c1,0,rect.height,c2);
+		Paint oldPaint = g2.getPaint();
+		
+		GradientPaint gp = new GradientPaint(0, y, top, 0, y+h-1, bottom);
 		g2.setPaint(gp);
-		g2.fillRect((int)rect.getX(), (int)rect.getY(), 
-				(int)rect.getWidth(), (int)rect.getHeight());
+		g2.fillRoundRect(x, y, w, h, arcWidth, arcHeight);
+		
+		g2.setPaint(oldPaint);
 	}
 	
 	
-	public static void drawRoundGradient(Graphics g, Rectangle rect, Color c1, Color c2) {
+	public static void drawBoxGradient(Graphics g, Rectangle rect, Color top, Color bottom) {
+		drawBoxGradient(g, rect.x, rect.y, rect.width, rect.height, top, bottom);
+	}
+	
+	
+	public static void drawBoxGradient(Graphics g, int x, int y, int w, int h, Color top, Color bottom) {
 		Graphics2D g2 = (Graphics2D)g;
-		GradientPaint gp = new GradientPaint(0,0,c1,0,rect.height,c2);
+		Paint oldPaint = g2.getPaint();
+		
+		GradientPaint gp = new GradientPaint(0, y, top, 0, y+h-1, bottom);
 		g2.setPaint(gp);
-		g2.fillOval((int)rect.getX(), (int)rect.getY(), 
-				(int)rect.getWidth(), (int)rect.getHeight());
+		g2.fillRect(x, y, w, h);
+		
+		g2.setPaint(oldPaint);
+	}
+	
+	
+	public static void drawRoundGradient(Graphics g, Rectangle rect, Color top, Color bottom) {
+		drawRoundGradient(g, rect.x, rect.y, rect.width, rect.height, top, bottom);
+	}
+	
+	
+	public static void drawRoundGradient(Graphics g, int x, int y, int w, int h, Color top, Color bottom) {
+		Graphics2D g2 = (Graphics2D)g;
+		Paint oldPaint = g2.getPaint();
+		
+		GradientPaint gp = new GradientPaint(0, y, top, 0, y+h-1, bottom);
+		g2.setPaint(gp);
+		g2.fillOval(x, y, w, h);
+		
+		g2.setPaint(oldPaint);
 	}
 }
