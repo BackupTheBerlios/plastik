160b7d96b5f4ed58093ee3c528b412242cd131b6
hampelratte <hampelratte>
2006-07-23 20:09:35 +0000

    menubar now paints background, too

--- plastik/src/de/hampelratte/plastik/PlastikMenuBarUI.java
+++ plastik/src/de/hampelratte/plastik/PlastikMenuBarUI.java
@@ -1,8 +1,13 @@
 package de.hampelratte.plastik;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+
 import javax.swing.JComponent;
 import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicMenuBarUI;
+
+import de.hampelratte.plastik.theme.PlastikColorTheme;
 
 public class PlastikMenuBarUI extends BasicMenuBarUI {
 	
@@ -13,4 +18,13 @@ public class PlastikMenuBarUI extends BasicMenuBarUI {
 		return new PlastikMenuBarUI();
     }
 	
+	
+	public void paint(Graphics g, JComponent c) {
+		Dimension d = c.getSize();
+		PlastikColorTheme colorTheme = PlastikLookAndFeel.getTheme().getColorTheme();
+		g.setColor(colorTheme.getColor(PlastikColorTheme.MENU_BAR | PlastikColorTheme.BACKGROUND));
+		g.fillRect(0, 0, d.width, d.height);
+		
+		super.paint(g,c);
+    }
 }
